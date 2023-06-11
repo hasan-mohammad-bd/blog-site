faf639da854fd6ee04263ca58d47cea1a8eb84e6
hasan-mohammad-bd <hasan.md.east@gmail.com>
2023-06-11 06:30:55 +0600

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+           <div>2023 Lamamia. All rights reserved.</div>
+           <div></div>
+        </footer>
+    );
+};
+
+export default Footer;
